6a9fdf8b29cf5e16621270271f77a4b9bfc0d320
Nata <81040260+Nataslamet@users.noreply.github.com>
2022-06-27 00:12:01 +0700

    Update sticker-pack.js

--- plugins/sticker-pack.js
+++ plugins/sticker-pack.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text }) => {
-  let res = 'https://api.zacros.my.id/search/sticker?query=pentol'
+  let res = 'https://api.lolhuman.xyz/api/stickerwa?apikey=eafa46a5dac2cd0cf3f0c2e1&query=meme'
   if (!res.ok) throw await 'Website Down'
   let json = await res.json()
   if (!json.result) throw json
